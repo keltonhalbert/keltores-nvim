ca2c7045cb203142df3659cf892f5e38c8472017
Kelton Halbert <kelton.halbert@noaa.gov>
2025-01-06 13:19:38 -0600

    added markdown rendering plugin, changed theme, and updated packages

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -65,4 +65,14 @@ return {
       },
     },
   },
+  {
+    "OXY2DEV/markview.nvim",
+    lazy = false, -- Recommended
+    -- ft = "markdown" -- If you decide to lazy-load anyway
+
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+  },
 }
